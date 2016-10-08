fdb24c5ea464aa905ec586ce04161a58973b6dd6
Roman Grechuk <GrechukRomanSergeevich@gmail.com>
2016-10-08 21:31:27 +0300

    GET /api/user/<id> - DONE

--- handlers/user.js
+++ handlers/user.js
@@ -1,7 +1,33 @@
 'use strict';
 
+const logger = require('../libs/logger')(module);
+const UserModel = require('../models/user');
+const errors = require('../errors');
+const _ = require('underscore');
+
 module.exports = function User() {
-    this.getById = function(req, res, next) {
-        res.send('User getById - req.param.id: ' + req.params.id);
+    this.getUserById = function(req, res, next) {
+        if (!req.decoded) return;
+
+        if (req.decoded._doc._id !== parseInt(req.params.id)) {
+            return next( new errors.HttpError(422, 'Verified user id does not match the requested', '_id') );
+        }
+
+        UserModel.findById(req.params.id, { password: 0, __v: 0 })
+            .exec((err, user) => {
+                if (err) {
+                    return next(err);
+                }
+
+                if (!user) {
+                    let err = new Error('User not found');
+                    err.status = 404;
+
+                    return next(err);
+                }
+
+                logger.info('It was updated user:\n' + user);
+                res.status(200).json(user);
+            });
     };
 };
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,8 @@ const express = require('express');
 const router = express.Router();
 const UserHandler = require('../handlers/user');
 const userHandler = new UserHandler();
+const mw = require('../middlewares');
 
-router.get('/:id', userHandler.getById);
+router.get('/:id', mw.verify, userHandler.getUserById);
 
 module.exports = router;
