1de251548d0ef28753ba8273a941ea824845c80a
roman <grechukromansergeevich@gmail.com>
2016-12-11 21:34:28 +0200

    Webpack v.1.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,76 @@
 'use strict';
 
-module.exports = {
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var helpers = require('./config/helpers');
 
+console.log('R helpers src: ---------------->', helpers.root('src'));
+
+module.exports = {
+    context: helpers.root('src'),
+
+    entry: {
+        polyfills: './polyfills.ts',
+        vendor: './vendor.ts',
+        app: './main.ts'
+    },
+
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+            },
+            {
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'),
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('src', 'app'),
+                loader: 'raw-loader'
+            }
+        ]
+    },
+
+
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
+
+        new ExtractTextPlugin('[name].css'),
+
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path: helpers.root('dist'),
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
 };
