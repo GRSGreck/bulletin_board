1be971f2cfe228677b6e173fbf2e7b3927512ac7
roman <grechukromansergeevich@gmail.com>
2017-01-22 18:49:12 +0200

    bootstrap 3 - DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as _ from 'lodash';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 import '../public/styles/styles.scss';
 
@@ -10,9 +11,32 @@ import '../public/styles/styles.scss';
 })
 
 export class AppComponent implements OnInit {
+
+    closeResult: string;
+
+    constructor(private modalService: NgbModal) {}
+
     ngOnInit() {
         console.log('process.env.ENV:', process.env.ENV);
 
         console.log('is number', _.isNumber(15));
+    }
+
+    open(content: any) {
+        this.modalService.open(content).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
     }
 }
